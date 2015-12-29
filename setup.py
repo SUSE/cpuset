SUSE/cpuset

commit 1341fdd07dfd82472d5df8f932b03038ae9be3be

Update URLs after project migration
Signed-off-by: Libor Pechacek <lpechacek@suse.com>

setup.py
@@ -10,7 +10,7 @@ setup(name = 'cpuset',
     license = 'GPLv2',
     author = 'Alex Tsariounov',
     author_email = 'alext@novell.com',
-    url = 'http://code.google.com/p/cpuset',
+    url = 'https://github.com/lpechacek/cpuset'
     description = 'Allows manipluation of cpusets and provides higher level functions.',
     long_description = \
         'Cpuset is a Python application to make using the cpusets facilities in the Linux\n'

README
@@ -3,15 +3,16 @@ standard Linux filesystem calls to make using the cpusets facilities
 in the Linux kernel easier.
 
 For the latest version see:
-   http://code.google.com/p/cpuset
+   https://github.com/lpechacek/cpuset
 
 For packages, see the OpenSUSE build service:
-   http://download.opensuse.org/repositories/home:/tsariounov:/cpuset
+   https://build.opensuse.org/package/show/hardware/cpuset
+   http://download.opensuse.org/repositories/hardware/
 
 Bugs or feature requests should be sent to:
-   http://code.google.com/p/cpuset/issues/list
- or for supported products, the Novell Bugzilla at:
-   http://bugzilla.novell.com
+   https://github.com/lpechacek/cpuset/issues
+ or for supported products, SUSE Bugzilla at:
+   https://bugzilla.suse.com
 
 -----
 

cpuset.spec
@@ -6,11 +6,11 @@
 # package are under the same license as the package itself.  
 #  
 # Please submit bugfixes or comments via 
-#        http://code.google.com/p/cpuset/issues/list
+#        https://github.com/lpechacek/cpuset/issues
 #    Or 
-#        http://bugs.opensuse.org/
+#        https://bugzilla.opensuse.org
 # 
-# For supported products, via http://bugzilla.novell.com
+# For supported products, via https://bugzilla.suse.com
 #  
 
 # norootforbuild  
@@ -18,9 +18,9 @@
 Name:           cpuset
 Version:        1.5.6
 Release:        1
-License:        GPL v2 only
+License:        GPL-2.0
 BuildRoot:      %{_tmppath}/%{name}-%{version}-build
-Url:            http://code.google.com/p/cpuset
+Url:            https://github.com/lpechacek/cpuset
 Group:          System/Management
 Summary:        Allows manipulation of cpusets on system and provides higher level functions
 Source:         %{name}-%{version}.tar.gz

commit 8ffce0b3d0d44e81f1ecb90e157e1504e8514a1a

Fix setup.py
Fixed regression introduced in 1341fdd07dfd8 plus a typo fix.

Signed-off-by: Libor Pechacek <lpechacek@suse.com>

setup.py
@@ -10,8 +10,8 @@ setup(name = 'cpuset',
     license = 'GPLv2',
     author = 'Alex Tsariounov',
     author_email = 'alext@novell.com',
-    url = 'https://github.com/lpechacek/cpuset'
-    description = 'Allows manipluation of cpusets and provides higher level functions.',
+    url = 'https://github.com/lpechacek/cpuset',
+    description = 'Allows manipulation of cpusets and provides higher level functions.',
     long_description = \
         'Cpuset is a Python application to make using the cpusets facilities in the Linux\n'
         'kernel easier. The actual included command is called cset and it allows\n'