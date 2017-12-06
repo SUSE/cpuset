011f52297d7708487c31293855db447bc4c3d9d0
Libor Pechacek <lpechacek@suse.com>
2017-12-06 10:24:15 +0100

    Update setup.py with new information
    Signed-off-by: Libor Pechacek <lpechacek@suse.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setup(name = 'cpuset',
     version = version,
     license = 'GPLv2',
     author = 'Alex Tsariounov',
-    author_email = 'alext@novell.com',
+    author_email = 'tsariounov@gmail.com',
+    maintainer = 'Libor Pechacek',
+    maintainer_email = 'lpechacek@suse.com',
     url = 'https://github.com/lpechacek/cpuset',
     description = 'Allows manipulation of cpusets and provides higher level functions.',
     long_description = \
@@ -17,6 +19,15 @@ setup(name = 'cpuset',
         'kernel easier. The actual included command is called cset and it allows\n'
         'manipulation of cpusets on the system and provides higher level functions such as\n'
         'implementation and control of a basic cpu shielding setup.',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Systems Administration',
+    ],
     scripts = ['cset'],
     packages = ['cpuset', 'cpuset.commands'],
     data_files = [
